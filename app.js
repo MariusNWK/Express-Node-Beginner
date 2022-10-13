98b45e66fa4f3dd41ff00a48de3a4f6113ee58d5
MariusNWK <marius.nowak@epitech.eu>
2022-10-13 15:17:54 +0200

    [ADD] middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const logger = require('morgan');
 const app = express();
 const port = 3000;
 
@@ -9,6 +10,22 @@ app.get('/', (req, res) => {
 const wiki = require('./wiki.js');
 
 app.use('/wiki', wiki);
+app.use(logger('dev'));
+
+// Un exemple de fonction middleware
+let a_middleware_function = function(req, res, next) {
+  console.log("Inside a_middleware_function");
+  next(); // Appelez next() pour qu'Express appelle la fonction middleware suivante dans la chaîne.
+}
+
+// Fonction ajoutée avec use() pour toutes les routes et verbes
+// app.use(a_middleware_function);
+
+// Fonction ajoutée avec use() pour une route spécifique
+// app.use('/middleware', a_middleware_function);
+
+// Une fonction middleware ajoutée pour un verbe (en l'occurrence get) et une route HTTP spécifiques
+app.get('/middleware', a_middleware_function);
 
 app.listen(port, () => {
   console.log(`Application exemple à l'écoute sur le port ${port}!`)
@@ -22,7 +39,7 @@ console.log('Premier');
 console.log('Second');
 
 // Asynchrone
-setTimeout(function() {
+setTimeout(function () {
   console.log('Dixième');
 }, 3000);
 console.log('Onzième');
